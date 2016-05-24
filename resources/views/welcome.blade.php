c55bfd2a42d8825a53eb1739456fa7adb0ffc5fd
Rado Sohpeak <radosopheak@gmail.com>
2016-05-24 15:27:28 +0700

    Change welcome page by Dodo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Welcome to learning git, folk</div>
             </div>
         </div>
     </body>
